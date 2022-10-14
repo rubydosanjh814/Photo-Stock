8cbe1775f3825ec5f7a9509682aa06d39fc4935c
Ruby <ruby.dosanjh814@gmail.com>
2022-10-14 00:33:13 -0600

    nav changes

--- nav.php
+++ nav.php
@@ -1,5 +1,5 @@
 <ul>
-  <li><a class="active" href="#home">Home</a></li>
+  <li><a class="" href="#home">Home</a></li>
   <li><a href="picture.php">Picture</a></li>
   <li><a href="category.php">Category</a></li>
   <li><a href="#about">About</a></li>
